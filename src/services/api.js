c6c80590ba4254e9c538d99b385c3ccaddd8faeb
Dua Tariq <duatariq243@gmail.com>
2026-02-16 11:56:00 -0500

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-business-assistant-frontend.onrender.com/",
+  baseURL: "https://ai-business-assistant-backend-49hu.onrender.com",
 });
 
 // Auth endpoints
